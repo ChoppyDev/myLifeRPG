e51761d3c563f97b1e19648d4942001c5039d73a
Nicolas KLEIN <nicolas6720@gmail.com>
2021-08-20 12:21:01 +0200

    Initial Commit

---
+++ MyLifeRPG/MyLifeRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLifeRPGApp.swift
+//  MyLifeRPG
+//
+//  Created by Nicolas Klein on 20/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyLifeRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
